1f4cddc8aac64d707e6edcbdc217386bd845453e
OfirBaram <ofirbaram@gmail.com>
2026-05-16 20:30:01 +0300

    עדכון

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-    API_URL: "YOUR_GOOGLE_SCRIPT_URL_HERE",
+    API_URL: "https://script.google.com/macros/s/AKfycbyFg6OMQqil9wjaDV4smilBCo9gXD0KImQKK6csADAei9yQ_-WFu9cGFng3MeDRrR_kWw/exec",
     VERSION: "2.0.0",
     IS_MOCK_MODE: true
 };
